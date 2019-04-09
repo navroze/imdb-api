520e5b93baa000131e66733f15932daad1370a1e
nbomanji-r7 <navroze_bomanji@rapid7.com>
2019-04-10 02:29:02 +0530

    set debug to false

--- imdb/settings.py
+++ imdb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'imdb-app-rest.herokuapp.com',
